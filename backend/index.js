c55a5d5fc3fb4a05d3a103fb2d5f16c29e65bb6c
Niket576 <niketchaudhary576@gmail.com>
2025-04-28 17:07:27 +0530

    changes in backend

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://moviesrm.onrender.com',
     credentials: true
 }
 app.use(cors(corsOptions));
